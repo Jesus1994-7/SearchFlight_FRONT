c3c6e26a59022b59016b422d4ba947b8883adf81
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-16 17:50:40 +0200

    Add views and components at App.jsx for routing

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Register from './views/register/register.jsx';
+import Login from './views/login/login.jsx';
+import Profile from './views/profile/profile.jsx';
+import Header from './components/header/header.jsx';
+import Footer from './components/footer/footer.jsx';
+import SearchFlight from './components/searchFlight/searchFlight.jsx';
+
+
 
 function App() {
   return (
