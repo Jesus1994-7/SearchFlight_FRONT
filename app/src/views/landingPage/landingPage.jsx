aef294824a0ded83a5d81922052cf260a8ed63b8
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-19 19:59:22 +0200

    Connection with back (gets)

--- app/src/views/landingPage/landingPage.jsx
+++ app/src/views/landingPage/landingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 
 import './landingPage.css'
@@ -7,7 +8,37 @@ class LandingPage extends React.Component {
     constructor(props){
         super(props);
 
+        this.state = {
+            questions: [],
+            iataCodes: [],
+            countries: [],
+            currencies: []
+        }
+        
+    }
 
+    //Llamadas del back almacenadas en una constante 
+    componentDidMount() {
+        axios.get(`http://localhost:3005/data/questions`)
+            .then( res => {
+                const questions = res.data;
+                this.setState({ questions });
+            })
+        axios.get(`http://localhost:3005/data/iatacodes`)
+            .then( res => {
+                const iataCodes = res.data;
+                this.setState({ iataCodes });
+            })
+        axios.get(`http://localhost:3005/data/countries`)
+            .then( res => {
+                const countries = res.data;
+                this.setState({ countries });
+            })
+        axios.get(`http://localhost:3005/data/currencies`)
+            .then( res => {
+                const currencies = res.data;
+                this.setState({ currencies });
+            }) 
     }
 
     render () {
