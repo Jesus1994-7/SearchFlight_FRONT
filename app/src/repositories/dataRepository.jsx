d6bc454ed99f87fe498f9b0eb2b7094c3543f608
Christian <chsanleo@gmail.com>
2020-07-20 23:54:11 +0200

    CreateAllMethodsDataRespository

--- app/src/repositories/dataRepository.jsx
+++ app/src/repositories/dataRepository.jsx
@@ -4,6 +4,33 @@ const EMPTY ='';
 
 class dataRepository {
 
-
+    async getAllQuestions() {
+        try {
+            const res = await axios.get(`http://localhost:3005/data/questions`);
+            return res.data;
+        }
+        catch (error) { return EMPTY; }
+    }
+    async getAllIataCodes() {
+        try {
+            const res = await axios.get(`http://localhost:3005/data/iatacodes`);
+            return res.data;
+        }
+        catch (error) { return EMPTY; }
+    }
+    async getAllCountries() {
+        try {
+            const res = await axios.get(`http://localhost:3005/data/countries`);
+            return res.data;
+        }
+        catch (error) { return EMPTY; }
+    }
+    async getAllCurrencies() {
+        try {
+            const res = await axios.get(`http://localhost:3005/data/currencies`);
+            return res.data;
+        }
+        catch (error) { return EMPTY; }
+    } 
 }
 export default dataRepository;
