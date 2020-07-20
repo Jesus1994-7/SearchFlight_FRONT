764ddfda8ab2d6d27fe29f0201d978ecf78b5cc0
Christian <chsanleo@gmail.com>
2020-07-21 01:19:13 +0200

    Create login in userService

--- app/src/services/userService.jsx
+++ app/src/services/userService.jsx
@@ -1,4 +1,12 @@
+import { login } from '../redux/actions.js';
+import userRepository from '../repositories/userRepository.jsx';
+
 class userService {
 
+    login(credentials) {
+        const logged = userRepository.login(credentials);
+        if(!logged){ throw Error("Could not log in.");}
+        login(logged);
+    }
 }
 export default userService;
