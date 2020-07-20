74567109e77b269220cb877a5276056cba1baa74
Christian <chsanleo@gmail.com>
2020-07-21 01:08:00 +0200

    Create default questions in utils

--- app/src/utils/utils.jsx
+++ app/src/utils/utils.jsx
@@ -5,6 +5,13 @@ class utils {
     isNullOrEmpty(variable){
         return (variable == null || variable ==="" || variable === " ");
     }
+    systemSecretQuestions() {
+        return [
+            {"questionSecret":"Nombre de tu primera mascota"},
+            {"questionSecret":"Modelo de tu primer coche"},
+            {"questionSecret":"Nombre de tu colegio"}
+        ];
+    }
 }
 
 export default utils;
