22badb9b0b4437bee69d9c4addda8efc1817f116
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-22 10:03:52 +0200

    Add CSS at header

--- app/src/components/header/header.jsx
+++ app/src/components/header/header.jsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
-import  user  from '../../redux/store.js'
+import user from '../../redux/store.js'
 
-import './header.css';
+import './header.scss';
+//ICONOS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faUser,faSearch } from '@fortawesome/free-solid-svg-icons'
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            isLoggedIn : false
+            isLoggedIn: false
         }
     };
 
     render() {
-        if(user ){
-            return(
-                <div className="comp-Header">
-                    <NavLink exact to="/">
-                        <img src="" alt=""/>
+        if (user) {
+            return (
+                <header className="comp-Header">
+                    <div><NavLink exact to="/">
+                        <img src="" alt="" />
                     </NavLink>
-                    <NavLink exact to="/login">
-                        <button>LOGIN</button> 
-                    </NavLink>
-                    <NavLink exact to="/register">
-                        <button>REGISTER</button> 
-                    </NavLink>
-                </div>
+                    </div>
+                    <div>
+                        <ul className="list-icons">
+                        <FontAwesomeIcon icon={faEnvelope} className="iconos"/> {/*CORREO*/}
+                            <li><FontAwesomeIcon  icon={faUser} className="iconos"/>  {/*CORREO*/}
+                            <ul>
+                                <li>
+                                <a href="/login" className="accesos">Login</a>
+                                <a href="/register" className="accesos">Register</a></li>
+                            </ul>
+                            </li>
+                            <li><FontAwesomeIcon icon={faSearch} className="iconos"/></li>
+                        </ul>
+                    </div>
+
+
+
+                </header>
             );
         }
-        }
-    };
+    }
+};
 
 export default Header;
