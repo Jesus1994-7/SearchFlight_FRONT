db9a3b1d84efd9617ce13558f3efac5737290321
Christian <chsanleo@gmail.com>
2020-07-23 00:13:20 +0200

    Add exchange component to landing page

--- app/src/views/landingPage/landingPage.jsx
+++ app/src/views/landingPage/landingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import Exchange from '../../components/exchange/exchange.jsx'
 import { dataService } from '../../services/dataService';
 
 import './landingPage.css';
@@ -30,6 +31,7 @@ class LandingPage extends React.Component {
                     <img src="" alt="" />
                     <img src="" alt="" />
                 </div>
+                <Exchange/>
                 <section className="offers">
                     <p>AeroFlot special offers</p>
                     <NavLink exact to="/offers">
