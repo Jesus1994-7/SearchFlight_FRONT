3ab54d3ea85bcc0b38d77e0d117b5517ec0eb1ef
Christian <chsanleo@gmail.com>
2020-07-23 00:00:01 +0200

    Create menu dropdown with values in redux Currencies

---
+++ app/src/components/currencyList/currencyList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './currencyList.css';
+
+import { connect } from 'react-redux';
+
+class CurrencyList extends React.Component {
+
+    state = {}
+
+    render() {
+        return (
+
+            <select defaultValue={this.props.selected} onChange={
+                e => this.props.setCurrency(
+                    e.target.value, this.props.id
+                )}>
+                {this.props.currencies?.map(currency => (
+                    <option key={currency.code}
+                        value={currency.code}
+                    >
+                        {currency.name}
+                    </option>
+                ))}
+            </select>
+
+        )
+    }
+}
+
+const mapStateToProps = ({ currenciesList }) => ({ currencies: currenciesList })
+export default connect(mapStateToProps)(CurrencyList);
