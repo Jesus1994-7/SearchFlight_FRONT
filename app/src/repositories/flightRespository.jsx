001682ccf1df81967213ce6d46cf144e360ac469
Christian <chsanleo@gmail.com>
2020-07-24 02:33:55 +0200

    Create method to recover flight data from back

--- app/src/repositories/flightRespository.jsx
+++ app/src/repositories/flightRespository.jsx
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
 export const flightRepository = {
-
+    getFlights
 };
+async function getFlights(dataFlights) {
+    try {
+        const res = await axios.post(`http://localhost:3005/data/searchFlight`, dataFlights);
+        return res.data;
+    } catch (error) {
+        throw Error(error);
+    }
+}
