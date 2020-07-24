02e0aeca473d7a86e1f47341a5e4294405e588d9
Christian <chsanleo@gmail.com>
2020-07-24 02:05:07 +0200

    Create airportList

---
+++ app/src/components/airportList/airportList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './airportList.css';
+
+import { connect } from 'react-redux';
+
+class AirportList extends React.Component {
+
+    state = {}
+
+    render() {
+        return (
+            <select defaultValue={this.props.selected} onChange={
+                e => this.props.setAirport(
+                    e.target.value, this.props.id
+                )}>
+                {this.props.airports?.map(airport => (
+                    <option key={airport.id}
+                        value={airport.id}
+                    >
+                        {airport.IataCode.code + " " + airport.name}
+                    </option>
+                ))}
+            </select>
+        )
+    }
+}
+
+const mapStateToProps = ({ airportsList }) => ({ airports: airportsList })
+export default connect(mapStateToProps)(AirportList);
