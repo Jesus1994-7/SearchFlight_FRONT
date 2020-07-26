7e1482284cb213ead56483977c03ca2e42adc28f
Christian <chsanleo@gmail.com>
2020-07-26 11:27:44 +0200

    Export method forgotPass

--- app/src/repositories/userRepository.js
+++ app/src/repositories/userRepository.js
@@ -4,6 +4,7 @@ export const userRepository = {
     login,
     updateUser,
     createUser,
+    forgotPassword
     //getUserbyToken
 };
 
@@ -11,7 +12,6 @@ async function login(credentials) {
     try {
         const res = await axios.post('main/login', credentials);
         return res.data;
-
     }
     catch (error) {
         throw Error("Could not log in.");
