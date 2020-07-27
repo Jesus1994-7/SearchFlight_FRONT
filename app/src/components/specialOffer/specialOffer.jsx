91ae288c5c9232e5dd37ab362609611f5784bd9a
Christian <chsanleo@gmail.com>
2020-07-27 03:23:51 +0200

    Create logic to specialOffer by landing

--- app/src/components/specialOffer/specialOffer.jsx
+++ app/src/components/specialOffer/specialOffer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import './specialOffer.scss';
+import AirportList from '../airportList/airportList.jsx';
 
+import './specialOffer.scss';
+import { utils } from '../../utils/utils.js';
 import { flightService } from '../../services/flightService.js';
 
 class SpecialOffer extends React.Component {
@@ -10,22 +12,37 @@ class SpecialOffer extends React.Component {
         super(props);
 
         this.state = {
+            airport : 0
         }
     }
 
     lastMinuteFlights = (ev) => {
         ev.preventDefault();
-
         let date = Date();
         flightService.getFlightsbyDate(date);
         this.props.history.push('/search');
     }
+    flightByAirport = (ev) => {
+        ev.preventDefault();
+        flightService.getFlightsbyLandingAirport(this.state.airport);
+        this.props.history.push('/search');
+    }
+    setAirport = (airport, id) => {
+        if(utils.isNullOrEmpty(airport)){return}
+        this.setState({airport:airport});
+    }
 
     render() {
         return (
-            <form onSubmit={this.lastMinuteFlights}>
-                <button type="submit">Last Minute!!</button>
-            </form>
+            <div>
+                <form onSubmit={this.lastMinuteFlights}>
+                    <button type="submit">Last Minute!!</button>
+                </form>
+                <form onSubmit={this.flightByAirport}>
+                <AirportList id={0} setAirport={this.setAirport} readOnly />
+                    <button type="submit">Quiero ir!</button>
+                </form>
+            </div>
         );
     }
 }
